cf47f9dd97093f656a502902da3890b448419aa9
philippineval <philippine.vallette@edu.ece.fr>
2025-04-08 18:06:30 +0200

    test database

--- src/dao/DatabaseConnection.java
+++ src/dao/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/puppyyogadb";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "mdp";
 
     public static Connection getConnection() throws SQLException {
         try {
